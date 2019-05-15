0e835dc37cdb00b75bbbec4aa3f9a0c2654f01bd
yungshun317 <seraph317@gmail.com>
2019-05-15 17:08:41 +0800

    feat: receive streamed images from the Kafka server and convert them to a Flask-readable format

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -16,6 +16,10 @@ def index():
 def video_feed():
     return Response(get_video_stream(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+def get_video_stream():
+    for msg in consumer:
+        yield (b'--frame\r\n' 
+               b'Content-Type: image/jpg\r\n\r\n' + msg.value + b'\r\n\r\n')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
