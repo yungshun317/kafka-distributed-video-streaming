7dcc2432f8cbc16cb826217c45a2b2b64f15bc6a
yungshun317 <seraph317@gmail.com>
2019-05-15 16:58:30 +0800

    feat: publish given video file to a specified Kafka topic

--- producer/producer.py
+++ producer/producer.py
@@ -3,10 +3,33 @@ from kafka import KafkaProducer
 
 topic = "testing"
 
+def publish_video(video_file):
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+    video = cv2.VideoCapture(video_file)
+
+    print("Publishing video...")
+
+    while (video.isOpened()):
+        success, frame = video.read()
+
+        if not success:
+            print("Bad read!")
+            break
+
+        ret, buffer = cv2.imencode(".jpg", frame)
+
+        producer.send(topic, buffer.tobytes())
+ 
+        time.sleep(0.2)
+
+    video.release()
+    print("Publish complete!")
+
 if __name__ == '__main__':
     if (len(sys.argv) > 1):
         video_path = sys.argv[1]
         publish_video(video_path)
     else:
         print("Publishing feed!")
-        publish_camera()
+        # publish_camera()
